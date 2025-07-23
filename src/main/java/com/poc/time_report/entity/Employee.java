19f210fc0965939aaccaf7aac860c8d70f21fb3b
namnguyen <bennguyen.working@gmail.com>
2025-07-23 07:28:52 +0700

    Initial project with Spring Boot

---
+++ src/main/java/com/poc/time_report/entity/Employee.java
@@ -0,0 +1,23 @@
+package com.poc.time_report.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employee")
+public class Employee {
+
+  @Id
+  private Long id;
+
+  @Column(length = 60)
+  private String name;
+}
